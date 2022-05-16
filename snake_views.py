14bb54bda5d5081154a1336e997b10dfdf82c3c0
coldani <46798787+coldani@users.noreply.github.com>
2022-05-16 19:26:47 +0100

    update dockstrings

--- snake_views.py
+++ snake_views.py
@@ -11,6 +11,8 @@ Color = Tuple[int, int, int]
 class Window:
 
     def __init__(self):
+        """Main view that is responsible for drawing all objects"""
+
         LEFT_PADDING: int = 1
         RIGHT_PADDING: int = 1
         TOP_PADDING: int = 3
@@ -39,6 +41,12 @@ class Window:
         pygame.display.set_caption(CAPTION)
 
     def update(self, model: SnakeModel):
+        """Updates the views by redrawing all objects
+
+        Args:
+            model (SnakeModel): SnakeModel from which relevant information on
+            game status and object positions are taken
+        """
         self.surface.fill(self.BACKGROUND_COLOR)
         self.game_surface.update()
         self.snake.update(model.snake_positions)
@@ -46,13 +54,13 @@ class Window:
         pygame.display.flip()
 
     def scale_position(self, position: Position) -> Position:
-        """Utility function that rescales object position
+        """Utility function that rescales an object position
 
         Args:
             position (Position): position in the grid
 
         Returns:
-            Position: position rescaled based on size of grid
+            Position: position rescaled on the correct pixel of the window
         """
         x, y = position
         x = x * GameSurface.GRID_SIZE + self.left_offset
@@ -65,7 +73,12 @@ class GameSurface:
     GRID_SIZE: int = 15
 
     def __init__(self, window: Window):
-        """Main view that is responsible for drawing all objects"""
+        """Game surface on which snake and apple are positioned
+
+        Args:
+            window (Window): The instance of Window in which GameSurfae is
+            positioned
+        """
         self.BACKGROUND_COLOR: Color = (240, 240, 240)
 
         self.window: Window = window
@@ -81,12 +94,7 @@ class GameSurface:
         pygame.draw.rect(self.window.surface, (0, 0, 0), self.surface, 1)
 
     def update(self):
-        """Update the views by redrawing all objects
-
-        Args:
-            model (SnakeModel): SnakeModel from which relevant information on
-            game status and object positions are taken
-        """
+        """Update the surface view"""
         pygame.draw.rect(
             self.window.surface, self.BACKGROUND_COLOR, self.surface, 0)
         pygame.draw.rect(self.window.surface, (0, 0, 0), self.surface, 1)
@@ -95,18 +103,40 @@ class GameSurface:
 class Snake:
 
     def __init__(self, window: Window):
+        """View responsible for drawing the Snake
+
+        Args:
+            window (Window): The instance of Window in which the snake is drawn
+        """
         self.HEAD_COLOR: Color = (220, 220, 20)
         self.COLOR: Color = (100, 220, 100)
 
         self.window: Window = window
 
     def is_inside_surface(self, position: Position) -> bool:
+        """Utility function that checks whether a given position is inside the
+        GameSurface grid
+
+        Args:
+            position (Position): a position relative to top-left of GameSurface
+            grid (i.e. (0, 0) is top-left corner)
+
+        Returns:
+            bool: True if position is inside GameSurface, False otherwise
+        """
         x = position[0]
         y = position[1]
         return (x >= 0 and y >= 0 and
                 x < GameSurface.WIDTH and y < GameSurface.HEIGHT)
 
     def update(self, positions: list[Position]):
+        """Updates the snake view
+
+        Args:
+            positions (list[Position]): list of unscaled positions of snake
+            head (first element) and body relative to top-left corner of
+            GameSurface
+        """
         for i, position in enumerate(positions):
             if self.is_inside_surface(position):
                 position = self.window.scale_position(position)
@@ -120,17 +150,39 @@ class Snake:
 class Apple:
 
     def __init__(self, window: Window):
+        """View responsible for drawing the Apple
+
+        Args:
+            window (Window): The instance of Window in which the apple is drawn
+        """
         self.COLOR: Color = (255, 50, 50)
         self.RADIUS: float = GameSurface.GRID_SIZE / 2
 
         self.window: Window = window
 
     def adjust_centre(self, centre: Position) -> Position:
+        """Utility function that adjust the centre of the apple by taking by
+        moving the passed Position by self.RADIUS to the right and to the
+        bottom
+
+        Args:
+            centre (Position): Position (in pixels, relative to top-left corner
+            of window)
+
+        Returns:
+            Position: Recentered position
+        """
         x = centre[0]
         y = centre[1]
         return (x+self.RADIUS, y+self.RADIUS)
 
     def update(self, centre: Position):
+        """Update the apple view
+
+        Args:
+            centre (Position): unscaled position of the centre relative to
+            top-left corner of GameSurface
+        """
         centre = self.window.scale_position(centre)
         centre = self.adjust_centre(centre)
         # draws circle
