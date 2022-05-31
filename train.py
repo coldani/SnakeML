ef16283f0939e46ff41b8c838f8a8cd1143eb64d
coldani <46798787+coldani@users.noreply.github.com>
2022-05-31 20:28:14 +0100

    save trained model to pickle

--- train.py
+++ train.py
@@ -1,4 +1,8 @@
 from genetic_algorithm import GeneticAlgorithm
+import pickle
 
-model = GeneticAlgorithm(100, [10, 10, 10])  # 200
-model.train(25, 3, 200, 1)  # 100
+model = GeneticAlgorithm(1000, [10, 10, 10])
+model.train(100, 3, 200, 1)
+
+with open("saved_models/model.pickle", "wb") as f:
+    pickle.dump(model, f)
